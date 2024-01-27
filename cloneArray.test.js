abf819d3db5e759ce7b738ea5306436af044c410
jmoncayo-pursuit <jmoncayo@pursuit.org>
2024-01-27 12:45:26 -0500

    files added

---
+++ cloneArray.test.js
@@ -0,0 +1,8 @@
+// const { clone } = require('@babel/types');
+const cloneArray = require('./cloneArray');
+
+test('properly clones array', () => {
+const array = [ 1, 2, 3];
+expect(cloneArray(array)).toEqual(array);
+expect(cloneArray(array)).not.toBe(array);
+}) 
---
+++ sum.js
@@ -0,0 +1,8 @@
+function sum (a, b) {
+    return a + b ;
+}
+function helper() {
+    console.long('Helper')
+}
+
+module.exports = sum;
